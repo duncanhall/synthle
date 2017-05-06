0e03a1c2cc2c0851de42bef83a8b6c3880bb81bb
Duncan Hall <himself@duncanhall.net>
2017-05-06 14:25:53 +0100

    Use express inplace of koa

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   head: {
+    title: 'starter',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
     link: [
-      {rel:'stylesheet', href:'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'}
+      { rel:'stylesheet', href:'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' }
     ]
   },
   css: [
-    { src: 'bulma', lang: 'sass' },
-    'assets/main.css'
+    { src: 'bulma', lang:'sass' },
+    '~assets/main.css'
   ]
 }
